98697df57cb05f11c68e77d8462bc9b61793dfcf
ahmed21222 <ried21225@gmail.com>
2025-03-04 15:52:11 +0200

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,7 +18,6 @@ Route::get('/about', function () {
     return view('about',compact('name','departments'));
 
 });
-
 Route::post('/about',function(){
     $name =$_POST['name'];
     $departments=[
@@ -26,4 +26,12 @@ Route::post('/about',function(){
         '3'=>'Sales',
     ];
     return view('about',compact('name','departments'));
+});
+Route::get('tasks',function(){
+    return view ('tasks');
+});
+Route::post('create',function(){
+    $task_name =$_POST['name'];
+    DB::table('tasks')->insert(['name' => $task_name]);
+    return view('tasks');
 });

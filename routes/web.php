1f715ea732599bf69d16363df013daa2741b7dab
ahmed riyad <61358582+ahmed21222@users.noreply.github.com>
2025-03-05 11:14:51 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,28 +8,27 @@ use App\Http\Controllers\UsersController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/about', function () {
-    $name='ahmed';
-    $departments=[
-        '1'=>'Tuchnical',
-        '2'=>'Finacial',
-        '3'=>'Sales',
-    ];
-    // return view('about', ['name' => $name]);
-    // return view('about')->with('name',$name);
-    return view('about',compact('name','departments'));
 
-});
-Route::post('/about',function(){
-    $name =$_POST['name'];
-    $departments=[
-        '1'=>'Tuchnical',
-        '2'=>'Finacial',
-        '3'=>'Sales',
+Route::get('/about', function () {
+    $name = 'ahmed';
+    $departments = [
+        '1' => 'Tuchnical',
+        '2' => 'Finacial',
+        '3' => 'Sales',
     ];
-    return view('about',compact('name','departments'));
+    return view('about', compact('name', 'departments'));
 });
-<<<<<<< HEAD
+
+Route::post('/about', function () {
+    $name = request('name');
+    $departments = [
+        '1' => 'Tuchnical',
+        '2' => 'Finacial',
+        '3' => 'Sales',
+    ];
+    return view('about', compact('name', 'departments'));
+});
+
 Route::get('tasks', function () {
     $tasks = DB::table('tasks')->get();
     return view('tasks', compact('tasks'));
@@ -40,34 +39,33 @@ Route::post('create', function () {
     DB::table('tasks')->insert(['name' => $task_name]);
     return redirect('tasks');
 });
+
 Route::post('delete/{id}', function ($id) {
     DB::table('tasks')->where('id', $id)->delete();
     return redirect()->back();
 });
+
 Route::post('edit/{id}', function ($id) {
-    $task=DB::table('tasks')->where('id', $id)->first();
-    $tasks=DB::table('tasks')->get();
-    return view('tasks', compact('task','tasks'));
+    $task = DB::table('tasks')->where('id', $id)->first();
+    $tasks = DB::table('tasks')->get();
+    return view('tasks', compact('task', 'tasks'));
 });
+
 Route::post('update', function () {
-    $id=$_POST['id'];
-    DB::table('tasks')->where('id','=', $id)->update(['name'=>$_POST['name']]);
+    $id = request('id');
+    DB::table('tasks')->where('id', '=', $id)->update(['name' => request('name')]);
     return redirect('tasks');
-=======
-
-
-Route::get('tasks', [taskController::class,'index']);
-Route::post('create',[taskController::class,'create']);
-Route::post('delete/{id}',[taskController::class,'destroy']);
-Route::post('edit/{id}',[taskController::class,'edit']);
-Route::post('update',[taskController::class,'update']);
-
-Route::get('app',function(){
-    return view('layouts.app');
->>>>>>> 1291520 (first commit)
 });
 
+Route::get('tasks', [taskController::class, 'index']);
+Route::post('create', [taskController::class, 'create']);
+Route::post('delete/{id}', [taskController::class, 'destroy']);
+Route::post('edit/{id}', [taskController::class, 'edit']);
+Route::post('update', [taskController::class, 'update']);
 
+Route::get('app', function () {
+    return view('layouts.app');
+});
 
 Route::get('/users', [UsersController::class, 'index']);
 Route::post('/users/create', [UsersController::class, 'create']);
